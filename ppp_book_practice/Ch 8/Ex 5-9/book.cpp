3d127f2cb76b54b41496f7461382c146f90dfc41
Lunaea <132797043+Lunaea@users.noreply.github.com>
2024-05-25 16:09:59 -0700

    Draft Ch 8.5-9 book class

---
+++ ppp_book_practice/Ch 8/Ex 5-9/book.cpp
@@ -0,0 +1,14 @@
+#include "book.h"
+#include "date.h"
+
+void Book::checkIn() {
+
+}
+
+void Book::checkOut() {
+
+}
+
+bool Book::isValid() const {
+
+}
---
+++ ppp_book_practice/Ch 8/Ex 5-9/book.h
@@ -0,0 +1,34 @@
+#ifndef BOOK_H
+#define BOOK_H
+
+#include "date.h"
+#include <string>
+#include <vector>
+
+class Book {
+public:
+    class InvalidBook {};
+
+    std::string getIsbn() const { return isbn; }
+    std::string getTitle() const { return title; }
+    std::string getAuthor() const { return author; }
+    Date getCopyright() const { return copyright; }
+    bool getStatus() const { return status; }
+
+    void checkIn();
+    void checkOut();
+    bool isValid() const;
+
+    Book(std::string i, std::string t, std::string a, Date d, bool s)
+        : isbn{ i }, title{ t }, author{ a }, copyright{ d }, status{ s }
+    {
+        if (!isValid())
+            throw InvalidBook{};
+    }
+
+private:
+    std::string isbn, title, author;
+    Date copyright{};
+    bool status{ true };
+};
+#endif
