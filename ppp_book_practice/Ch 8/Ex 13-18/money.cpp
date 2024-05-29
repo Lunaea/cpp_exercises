1055b085b95bc445f79071c9361762828b558b3a
Lunaea <132797043+Lunaea@users.noreply.github.com>
2024-05-29 12:49:51 -0700

    Implement overloaded operator>> and accept currency denomination

--- ppp_book_practice/Ch 8/Ex 13-18/money.cpp
+++ ppp_book_practice/Ch 8/Ex 13-18/money.cpp
@@ -1,11 +1,29 @@
 #include "money.h"
 
 std::ostream& operator<<(std::ostream& o, Money& a) {
-    o << '$' << a.getDollars() << '.';
+    o << a.getCurrency() << a.getDollars() << '.';
     if (a.getCents() == 0)
         o << "00";
     else
         o << a.getCents();
 
     return o;
+}
+
+std::istream& operator>>(std::istream& i, Money& a) {
+    char ch{};
+    std::string s{};
+    float x{};
+
+    while (i.get(ch)) {
+        if (isalpha(ch))
+            s += ch;
+        else {
+            i.putback(ch);
+            i >> x;
+        }
+    }
+
+    a = Money{ s, x };
+    return i;
 }
--- ppp_book_practice/Ch 8/Ex 13-18/money.h
+++ ppp_book_practice/Ch 8/Ex 13-18/money.h
@@ -3,24 +3,30 @@
 
 #include <iostream>
 #include <cmath>
+#include <string>
 
 class Money {
 public:
     Money() {}
-    Money(long c): total{ c } 
+    Money(std::string s, float c): currency{ s } 
     {
+        total = std::round(c * 100);
         dollars = total / 100;
         cents = total % 100;
     }
 
+    const std::string& getCurrency() const { return currency; }
     int getCents() const { return cents; }
     long getDollars() const { return dollars; }
     long getTotal() const { return total; }
+
+    friend std::istream& operator>>(std::istream& i, Money& a);
 
 private:
     int cents{};
     long dollars{};
     long total{};
+    std::string currency{};
 };
 
 std::ostream& operator<<(std::ostream& o, Money& a);
