95f4378e2be275d9bb2f9deaa6933d4d71f3fe92
Lunaea <132797043+Lunaea@users.noreply.github.com>
2024-08-11 08:23:24 -0700

    Complete Ch 9 Ex 3

---
+++ ppp_book_practice/Ch 9/Ex 3/Ex 3.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+const std::vector<char> vowels{ 'a', 'e', 'i', 'o', 'u' };
+
+bool isvowel(char c) {
+    char x = tolower(c);
+
+    for (char v : vowels)
+        if (x == v)
+            return true;
+
+    return false;
+}
+
+int main() {
+    std::string name{};
+    std::cout << "Please enter a file name: ";
+    std::getline(std::cin, name);
+
+    std::ifstream ifs{name};
+    if(!ifs)
+        throw std::runtime_error("Unable to open file " + name);
+
+    std::ofstream ofs{"output.txt"};
+
+    std::vector<std::string> lines{};
+    for (std::string line; std::getline(ifs, line);)
+        lines.push_back(line);
+
+    for (int i = 0; i < lines.size(); ++i) {
+        for (int j = 0; j < lines[i].size(); ++j) {
+            if (isvowel(lines[i][j])) {
+                lines[i].erase(j, 1);
+                --j;    //decrement j to account for now removed vowel
+            }
+        }
+    }
+
+    for (auto line : lines)
+        ofs << line << '\n';
+
+    return 0;
+}
