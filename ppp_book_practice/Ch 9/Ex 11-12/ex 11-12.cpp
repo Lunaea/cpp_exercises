a891b8b41dfccaf9fc7808e72c354e0abfddcf84
Lunaea <132797043+Lunaea@users.noreply.github.com>
2024-09-09 03:36:32 -0700

    Complete Ch 9 Ex 12

--- ppp_book_practice/Ch 9/Ex 11-12/ex 11-12.cpp
+++ ppp_book_practice/Ch 9/Ex 11-12/ex 11-12.cpp
@@ -4,7 +4,7 @@
 #include <fstream>
 
 void reverseChars(const std::vector<std::string>& lines, std::vector<std::string>& reversed) {
-    for(int i = lines.size() -1; i >= 0; --i) {
+    for(int i = lines.size() - 1; i >= 0; --i) {
         std::string reverse{};
         for(int j = lines[i].length() - 1; j >= 0; --j) {
             reverse += lines[i][j];
@@ -13,21 +13,39 @@ void reverseChars(const std::vector<std::string>& lines, std::vector<std::string
     }
 }
 
+void reverseWords(const std::vector<std::string>& words, std::vector<std::string>& reversed) {
+    for(int i = words.size() - 1; i >= 0; --i)
+        reversed.push_back(words[i]);
+}
+
 int main() {
     std::ifstream ist{ "input.txt" };
-    std::ofstream ost{ "output.txt" };
+    std::ofstream ost{ "outputChar.txt" };
+    std::ofstream ost2{ "outputWord.txt" };
 
     std::vector<std::string> lines{};
+    std::vector<std::string> words{};
 
     for(std::string line; std::getline(ist, line);)
         lines.push_back(line);
 
-    std::vector<std::string> reversed{};
+    ist.clear(); //clear ist eof state
+    ist.seekg(0, std::ios::beg); //seek to beginning of ist
+    
+    for(std::string word; ist >> word;)
+        words.push_back(word);
 
-    reverseChars(lines, reversed);
+    std::vector<std::string> reversedChar{};
+    std::vector<std::string> reversedWord{};
 
-    for(auto line : reversed)
+    reverseChars(lines, reversedChar);
+    reverseWords(words, reversedWord);
+
+    for(auto line : reversedChar)
         ost << line << '\n';
+
+    for(auto word : reversedWord)
+        ost2 << word << ' ';
 
     return 0;
 }
